eda34d874173131ca621d41fa3fc24f5c6eeb134
Tanmay Sharma <tanmaysharmav@gmail.com>
2021-08-16 11:45:09 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from remainder import remainder
 from datetime import datetime
 from discord import client
 
+
 client = discord.Client()
 
 
@@ -17,6 +18,76 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('-----')
+    
+    
+@client.event
+async def on_member_join(member):
+    #guild = client.get_guild(850079276102844449)
+    #channel = guild.get_channel(850079276102844452)
+    
+    guild = member.guild
+    channel = discord.utils.get(guild.channels, name = "general")
+    #channel = member.channel
+    #await channel.send(f'{member.mention} just joined the server !! :grin: ')
+    embed=discord.Embed(
+        title="Welcome "+member.name+"!",
+        description="This is the GCC Code Camp discord server!",
+        color=0x0000FF,
+        timestamp=datetime.utcnow()
+    )
+    embed.add_field(name="Name", 
+                    value=member.mention, 
+                    inline=True)
+    embed.add_field(name="Who am I ?", 
+                    value=f'Hello Adventurer !! I am the GuviBot. I was made for the sole purpose of helping you out in this amazing journey of yours with the GCC. So come along, as your very own adventure awaits!!!! ',
+                    inline=False)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_author(name="GUVI Code Camp",
+                     url="https://www.gccatsrm.tech/",
+                     icon_url=guild.icon_url)
+    embed.set_footer(text="                  ")
+    embed.set_image(url="https://media.giphy.com/media/IgGcxqawkRc6y43Z6I/giphy.gif")
+    #await channel.send(embed=embed)
+    try:
+        #await message.send_message(discord.utils.get(guild.channels, name = "general"), embed=embed)
+        await channel.send(embed=embed)
+    except discord.InvalidArgument:
+        #await guild.create_channel(guild, "general", type=discord.ChannelType.text)
+        #await message.send_message(discord.utils.get(guild.channels, name = "channel_name"), embed=embed)
+        print("Channel doesn't exist")
+
+    await member.send(f'Hello {member.mention} !! I am the GuviBot. :robot: ')
+    await member.send(f'You have just joined -- {guild.name}. Have Fun !! :partying_face: !!')
+    
+    
+    @client.event
+async def on_member_leave(member):
+    #guild = client.get_guild(850079276102844449)
+    #channel = guild.get_channel(850079276102844452)
+    guild = member.guild
+    channel = discord.utils.get(guild.channels, name = "general")
+
+    embed=discord.Embed(
+        title="Adios !! "+member.name+"!",
+        description="It's sad but we will move on :cry: ",
+        color=0x0000FF
+    )
+    embed.add_field(name="Name", 
+                    value=member.mention, 
+                    inline=True)
+    embed.add_field(name="Message", 
+                    value=f'Have a nice life!! ', 
+                    inline=True)
+    embed.set_thumbnail(url=member.avatar_url)
+    #await channel.send(embed=embed)
+
+    try:
+        #await message.send_message(discord.utils.get(guild.channels, name = "general"), embed=embed)
+        await channel.send(embed=embed)
+    except discord.InvalidArgument:
+        #await guild.create_channel(guild, "general", type=discord.ChannelType.text)
+        #await message.send_message(discord.utils.get(guild.channels, name = "channel_name"), embed=embed)
+        print("Channel doesn't exist")
 
 
 @client.event
@@ -32,54 +103,6 @@ async def on_message(message):
         await meetcmd.meet(message)
     elif message.content.startswith('&member'):
         await member.membr(message)
-        
 
-@client.event
-async def on_member_join(member):
-    guild = client.get_guild(850079276102844449)
-    channel = guild.get_channel(850079276102844452)
-    await channel.send(f'{member.mention} just joined the server !! :grin: ')
-    embed=discord.Embed(
-        title="Welcome "+member.name+"!",
-        description="This is the GCC Code Camp discord server!",
-        color=0x0000FF,
-        timestamp=datetime.utcnow()
-    )
-    embed.add_field(name="Name", 
-                    value=member.mention, 
-                    inline=True)
-    embed.add_field(name="Who am I ?", 
-                    value=f'Hello Adventurer !! I am the GuviBot. I was made for the sole purpose of helping you out in this amazing journey of yours with the GCC. So come along, as you very own adventure awaits!!!! ',
-                    inline=False)
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.set_author(name="GUVI Code Camp",
-                     url="https://www.gccatsrm.tech/",
-                     icon_url=guild.icon_url)
-    embed.set_footer(text="                  ")
-    embed.set_image(url="https://media.giphy.com/media/IgGcxqawkRc6y43Z6I/giphy.gif")
-    await channel.send(embed=embed)
-
-    await member.send(f'Hello {member.mention} !! I am the GuviBot. :robot: ')
-    await member.send(f'You have just joined -- {guild.name}. Have Fun !! :partying_face: !!')
-
-
-@client.event
-async def on_member_leave(member):
-    guild = client.get_guild(850079276102844449)
-    channel = guild.get_channel(850079276102844452)
-    embed=discord.Embed(
-        title="Adios !! "+member.name+"!",
-        description="It's sad but we will move on :cry: ",
-        color=0x0000FF
-    )
-    embed.add_field(name="Name", 
-                    value=member.mention, 
-                    inline=True)
-    embed.add_field(name="Message", 
-                    value=f'Have a nice life!! ', 
-                    inline=True)
-    embed.set_thumbnail(url=member.avatar_url)
-    await channel.send(embed=embed)
-        
         
 client.run('ODQ5NjMyNjYwODkyNTQ5MTIx.YLeAEw.NySyt8fFI66QTgUXq0EKUGtB10M')
