09d99796563408434e79924d80f35cca37150415
Pradipta Nandi <78438978+ProMinato500@users.noreply.github.com>
2021-08-17 10:42:54 +0530

    Update remainder.py

--- src/remainder/remainder.py
+++ src/remainder/remainder.py
@@ -23,10 +23,10 @@ async def remainder(Client):
                 print(2)
                 id = eachMeet["ids"]
                 presentTime = datetime.datetime.now().timestamp()
-                if presentTime > eachMeet["TimeStamp"]-1800 and eachMeet["Reminder"] == 2:
+                if presentTime > eachMeet["TimeStamp"]-1800:
                     Task_details.Data.update_one({"ids" : str(id)},{ "$inc" : {"Reminder": -1} })
                     await premeetReminder.send_before_reminder(Client, eachMeet)
-                if presentTime > eachMeet["TimeStamp"] and eachMeet["Reminder"] == 1:
+                if presentTime > eachMeet["TimeStamp"]:
                     await postmeetReminder.send_after_reminder(Client, eachMeet)
                     Task_details.Data.delete_one(eachMeet)
                     print("Meet time")
